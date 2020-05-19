467a011355a224cc30577be4be3b213a8df361c4
wzj <wzj@dd963fbc-58d4-4d45-9bdf-300ffcb5158c>
2020-05-19 06:23:07 +0000

    git-svn-id: https://39.108.144.9:8443/svn/haircrm@50 dd963fbc-58d4-4d45-9bdf-300ffcb5158c

--- public/themes/salesman/views/order/handle_cart.blade.php
+++ public/themes/salesman/views/order/handle_cart.blade.php
@@ -86,7 +86,7 @@
             //运费四舍五入
             //freight = Math.round(freight);
             //paypal取整，去小数
-            paypal_fee = parseInt((selling_price + freight) * parseFloat("{{ setting('paypal_fee') }}"));
+            paypal_fee = parseInt(((selling_price + freight) * parseFloat("{{ setting('paypal_fee') }}")) * 100)/100;
             total = parseFloat(selling_price) + parseFloat(freight) + parseFloat(paypal_fee);
             $("#weight").text(weight.toFixed(3));
             $("#selling_price").text(selling_price);
