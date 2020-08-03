8332d7ffc7513feb8400af599d53cb918070d907
wzj <wzj@dd963fbc-58d4-4d45-9bdf-300ffcb5158c>
2020-08-03 09:29:32 +0000

    git-svn-id: https://39.108.144.9:8443/svn/haircrm@106 dd963fbc-58d4-4d45-9bdf-300ffcb5158c

--- public/themes/admin/views/mail_schedule/show.blade.php
+++ public/themes/admin/views/mail_schedule/show.blade.php
@@ -3,79 +3,55 @@
         <div class="layui-breadcrumb" lay-filter="breadcrumb" style="visibility: visible;">
             <a href="{{ guard_url('home') }}">{{ trans('app.home') }}</a><span lay-separator="">/</span>
             <a href="{{ route('mail_schedule.index') }}"><cite>{{ trans('mail_schedule.title') }}</cite></a><span lay-separator="">/</span>
-            <a><cite>{{ trans('app.add') }}{{ trans('mail_schedule.name') }}</cite></a>
+            <a><cite>{{ trans('app.details') }}{{ trans('mail_schedule.name') }}</cite></a>
         </div>
     </div>
     <div class="main_full">
         {!! Theme::partial('message') !!}
         <div class="layui-col-md12">
             <div class="fb-main-table">
-                <form class="layui-form" action="{{guard_url('mail_schedule/'.$mail_schedule->id)}}" method="post" lay-filter="fb-form">
 
-                    <div class="layui-form-item fb-form-item2">
-                        <label class="layui-form-label">{{ trans('app.title') }} *</label>
-                        <div class="layui-input-block">
-                            <input type="text" name="title" lay-verify="required" autocomplete="off" placeholder="请输入{{ trans('app.title') }}" class="layui-input" value="{{ $mail_schedule->title }}">
-                        </div>
-                    </div>
-                    <div class="layui-form-item fb-form-item2">
-                        <label class="layui-form-label">{{ trans('mail_schedule.label.interval') }} *</label>
-                        <div class="layui-input-block">
-                            <input type="text" name="interval" lay-verify="required" autocomplete="off" placeholder="请输入{{ trans('mail_schedule.label.interval') }}" class="layui-input" value="{{ config('model.mail.mail_schedule.interval') }}">
-                        </div>
-                    </div>
-
-                    <div class="layui-form-item fb-form-item2">
-                        <label class="layui-form-label">{{ trans('mail_schedule.label.per_hour_mail') }} *</label>
-                        <div class="layui-input-block">
-                            <input type="text" name="per_hour_mail" lay-verify="required" autocomplete="off" placeholder="请输入{{ trans('mail_schedule.label.per_hour_mail') }}" class="layui-input" value="{{ config('model.mail.mail_schedule.per_hour_mail') }}">
-                        </div>
-                    </div>
-
-
-                    <div class="layui-form-item">
-                        <label class="layui-form-label">{{ trans('mail_account.name') }} *</label>
-
-                        <div class="layui-input-block">
-                            @inject('mailAccountRepository','App\Repositories\Eloquent\MailAccountRepository')
-                            @foreach( $mailAccountRepository->getAll() as $key => $mail_account)
-                                <input type="checkbox" name="mail_accounts[]"  title="{{ $mail_account->username }}" value="{{ $mail_account->id }}" @if(in_array($mail_account->id ,$mail_account_ids)) checked @endif>
-                            @endforeach
-
-                        </div>
-                    </div>
-                    <div class="layui-form-item">
-                        <label class="layui-form-label">{{ trans('mail_template.name') }} *</label>
-
-                        <div class="layui-input-block">
-                            @inject('mailTemplateRepository','App\Repositories\Eloquent\MailTemplateRepository')
-                            @foreach( $mailTemplateRepository->getAll() as $key => $mail_template)
-                                <input type="checkbox" name="mail_templates[]" title="{{ $mail_template->name }}" value="{{ $mail_template->id }}" @if(in_array($mail_template->id ,$mail_template_ids)) checked @endif>
-                            @endforeach
-
-                        </div>
-                    </div>
-
-                    <div class="layui-form-item fb-form-item2">
-                        <label class="layui-form-label">{{ trans('mail_schedule.label.active') }} *</label>
-
-                        <div class="layui-input-block">
-                            <input type="checkbox" name="active" value="1" lay-skin="switch" lay-text="是|否" lay-filter="active" checked>
-                        </div>
-
-                    </div>
-
-                    <div class="layui-form-item">
-                        <div class="layui-input-block">
-                            <button class="layui-btn layui-btn-submit" lay-submit="" lay-filter="demo1">立即提交</button>
-                        </div>
-                    </div>
-                    <input type="hidden" name="_method" value="PUT">
-                    {!!Form::token()!!}
-                </form>
             </div>
+            <div class="table-Box">
+                <table class="layui-table" lay-filter="fb-table" id="fb-table">
 
+                </table>
+            </div>
         </div>
     </div>
 </div>
+<script>
+    var main_url = "{{guard_url('mail_schedule_report')}}";
+    var delete_all_url = "{{guard_url('mail_schedule_report/destroyAll')}}";
+    layui.use(['jquery','element','table'], function(){
+        var $ = layui.$;
+        var table = layui.table;
+        var form = layui.form;
+        var element = layui.element;
+        table.render({
+            elem: '#fb-table'
+            ,url: '{{guard_url('mail_schedule_report')}}?mail_schedule_id={{ $mail_schedule->id }}'
+            ,cols: [[
+                {checkbox: true, fixed: true}
+                ,{field:'id',title:'ID', width:80,sort:true}
+                ,{field:'email',title:'{{ trans('mail_schedule_report.label.email') }}', width:220}
+                ,{field:'sent_desc',title:'{{ trans('mail_schedule_report.label.sent') }}', width:100}
+                ,{field:'status_desc',title:'{{ trans('mail_schedule_report.label.status') }}'}
+                ,{field:'mail_account_username',title:'{{ trans('mail_schedule_report.label.mail_account_username') }}'}
+                ,{field:'mail_template_name',title:'{{ trans('mail_schedule_report.label.mail_template_name') }}'}
+                ,{field:'send_at',title:'{{ trans('mail_schedule_report.label.send_at') }}', width:180}
+                ,{field:'mail_return',title:'{{ trans('mail_schedule_report.label.mail_return') }}'}
+            ]]
+            ,id: 'fb-table'
+            ,page: true
+            ,limit: '{{ config('app.limit') }}'
+            ,height: 'full-200'
+            ,cellMinWidth :'160'
+            ,done:function () {
+                element.init();
+            }
+        });
 
+    });
+
+</script>
