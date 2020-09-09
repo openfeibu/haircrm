9a52db2e8cfccc8e2902cfecbc3e82def89a8dae
wzj <wzj@dd963fbc-58d4-4d45-9bdf-300ffcb5158c>
2020-09-09 10:07:52 +0000

    git-svn-id: https://39.108.144.9:8443/svn/haircrm@128 dd963fbc-58d4-4d45-9bdf-300ffcb5158c

--- app/Services/MailScheduleService.php
+++ app/Services/MailScheduleService.php
@@ -32,8 +32,8 @@ class MailScheduleService
          *  per_hour_mail < mail_schedule_reports 表，sent = 1 ，一个小时内发的数量, 不发送
          */
 
-        if(!(date('G')<12 || date('G')>18)){
-            return '只在早上12点前，晚上7点后发送,其他时间段内不发送.';
+        if(date('G')>12 && date('G')<18){
+            return '只在早上12点前，晚上6点后发送,其他时间段内不发送.';
         }
         $schedule = MailSchedule::where('active',1)->whereNotIn('status',['complete'])->first();
         if(!$schedule)
