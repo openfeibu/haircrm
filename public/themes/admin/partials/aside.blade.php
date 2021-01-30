7cff3f0ade85ec60c4eef515a93f585ec12de489
wzj <wzj@dd963fbc-58d4-4d45-9bdf-300ffcb5158c>
2021-01-30 09:57:22 +0000

    git-svn-id: https://39.108.144.9:8443/svn/haircrm@172 dd963fbc-58d4-4d45-9bdf-300ffcb5158c

--- public/themes/admin/partials/aside.blade.php
+++ public/themes/admin/partials/aside.blade.php
@@ -1,6 +1,6 @@
 <div class="layui-side layui-side-menu">
     <div class="layui-side-scroll">
-        <div class="logo"><img src="/images/hairCrmLogo.png" /></div>
+        <div class="logo"><img src="{{ url('/image/original').setting('logo') }}" /></div>
         <ul class="layui-nav layui-nav-tree" lay-filter="test">
             {{--{!!Menu::menu('admin', 'menu.menu.admin')!!}--}}
             @inject('permissionPresenter','App\Repositories\Presenter\PermissionPresenter')
