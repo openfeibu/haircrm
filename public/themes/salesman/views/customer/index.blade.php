e8bff3801ed6ae176984f1e22f854cd46d484626
wzj <wzj@dd963fbc-58d4-4d45-9bdf-300ffcb5158c>
2021-07-01 14:31:26 +0000

    git-svn-id: https://39.108.144.9:8443/svn/haircrm@228 dd963fbc-58d4-4d45-9bdf-300ffcb5158c

--- public/themes/salesman/views/customer/index.blade.php
+++ public/themes/salesman/views/customer/index.blade.php
@@ -76,7 +76,7 @@
                 ,{field:'id',title:'ID', width:80}
                 ,{field:'name',title:'{{ trans('customer.label.name') }}',edit:'text'}
                 ,{field:'stage_desc',title:'{{ trans('customer.label.stage') }}'}
-                ,{field:'ig',title:'{{ trans('customer.label.ig') }}',templet:'<div><a href="https://www.instagram.com/@{{ d.ig }}" target="_blank">@{{ d.ig }}</a></div>'}
+                ,{field:'ig',title:'{{ trans('customer.label.ig') }}',templet:'<div>@{{# if(d.ig){ }}<a href="https://www.instagram.com/@{{ d.ig }}" target="_blank">@{{ d.ig }}</a>@{{# }  }}</div>'}
                 ,{field:'from',title:'{{ trans('customer.label.from') }}',edit:'text'}
                 ,{field:'email',title:'{{ trans('customer.label.email') }}',edit:'text'}
                 ,{field:'mobile',title:'{{ trans('customer.label.mobile') }}',edit:'text'}
