d4e1cdcc6f27d3b47cc7e54d48031903650b566f
wzj <wzj@dd963fbc-58d4-4d45-9bdf-300ffcb5158c>
2022-10-05 11:24:35 +0000

    git-svn-id: https://39.108.144.9:8443/svn/haircrm@262 dd963fbc-58d4-4d45-9bdf-300ffcb5158c

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -36,7 +36,7 @@ class Kernel extends ConsoleKernel
         //          ->hourly();
         $schedule->command('mail_schedule:auto')->everyMinute();
         $schedule->command('pricing:auto')->everyFiveMinutes();
-        $schedule->command('restore_price:auto')->dailyAt('7:00');
+        $schedule->command('restore_price:auto')->dailyAt('07:00');
     }
 
     /**
