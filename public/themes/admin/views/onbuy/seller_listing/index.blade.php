3964d2c1ce56aa4c3f282e627af2541a07a25564
wzj <wzj@dd963fbc-58d4-4d45-9bdf-300ffcb5158c>
2022-12-29 08:43:36 +0000

    git-svn-id: https://39.108.144.9:8443/svn/haircrm@344 dd963fbc-58d4-4d45-9bdf-300ffcb5158c

--- public/themes/admin/views/onbuy/seller_listing/index.blade.php
+++ public/themes/admin/views/onbuy/seller_listing/index.blade.php
@@ -49,13 +49,14 @@
 </div>
 <script type="text/html" id="barDemo">
     <p>
-    <a class="layui-btn layui-btn-sm" href="{{ guard_url('goods') }}/@{{ d.goods_id }}">{{ trans('app.edit') }}</a>
+        <a class="layui-btn layui-btn-sm" href="{{ guard_url('goods') }}/@{{ d.goods_id }}">{{ trans('app.edit') }}</a>
 
-    <a class="layui-btn layui-btn-danger layui-btn-sm" lay-event="del">{{ trans('app.delete') }}</a>
+        <a class="layui-btn layui-btn-danger layui-btn-sm" lay-event="del">{{ trans('app.delete') }}</a>
     </p>
     <p>
-    <a class="layui-btn layui-btn-warm layui-btn-sm" lay-event="in_inventory">进货</a>
-    <a class="layui-btn layui-btn-danger layui-btn-sm" lay-event="out_inventory">出货</a>
+        <a class="layui-btn layui-btn-warm layui-btn-sm" lay-event="in_inventory">进货</a>
+        <a class="layui-btn layui-btn-danger layui-btn-sm" lay-event="out_inventory">出货</a>
+        <a class="layui-btn layui-btn-normal layui-btn-sm" href="{{ guard_url('onbuy/order/') }}?search[onbuy_order_products.sku]=@{{ d.sku }}" target="_blank">订单</a>
     </p>
 </script>
 <script type="text/html" id="imageTEM">
